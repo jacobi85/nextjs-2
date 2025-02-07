cf27a4a9d80eda6b09170548d7626a480eb23394
jacobi85 <hello@jacobwestman.com>
2025-02-07 21:33:53 +0100

    added styles using css modules

--- src/app/blog/about/page.tsx
+++ src/app/blog/about/page.tsx
@@ -1,8 +1,29 @@
-export default function AboutPage() {
+export default function Page() {
   return (
-    <>
-      <h1>About</h1>
-      <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
-    </>)
-
+    <div>
+      <h1 className="text-purple-800">Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, architecto
+        natus, cumque laborum, error consequuntur eaque expedita numquam eveniet
+        quas sunt possimus eligendi minus omnis sapiente sequi cum magni
+        distinctio?
+      </p>
+      <div>
+        <h2>Get in Touch</h2>
+        <ul style={{ listStyleType: "square" }}>
+          <li>
+            <span>
+              Email:{" "}
+              <a href="mailto:contact@example.com">contact@example.com</a>
+            </span>
+          </li>
+          <li>
+            <span>
+              Website: <a href="https://www.example.com">www.example.com</a>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styles from "@/app/ui/styles/home.module.css";
 
 export default function Home() {
   return (
@@ -6,14 +7,16 @@ export default function Home() {
       <div className="container mx-auto p-4 relative">
         <div className="bg-white border-2 border-purple-100 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col justify-center">
-            <h1 className="text-4xl text-purple-700 font-bold mb-4">Welcome</h1>
+            <h1 className={`text-4xl font-bold mb-4 ${styles.text_wisteria}`}>
+              Welcome
+            </h1>
             <p className="text-lg text-gray-700 mb-4">
               Massa urna magnis dignissim id euismod porttitor vitae etiam
               viverra nunc at adipiscing sit morbi aliquet mauris porttitor
               nisi, senectus pharetra, ac porttitor orci.
             </p>
             <Link
-              className="outline outline-1 outline-offset-2 border-purple-700 text-purple-700 hover:text-white py-2 px-4 rounded hover:bg-purple-800 md:w-auto"
+              className={`outline outline-1 outline-offset-2 border-purple-700 text-purple-700 hover:text-white py-2 px-4 rounded hover:bg-purple-800 md:w-auto ${styles.fit_content}`}
               href={"/blog/posts"}
             >
               Go to Blog
@@ -21,7 +24,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-purple-800 hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0"></div>
+      <div
+        className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_wisteria}`}
+      ></div>
     </main>
   );
 }
