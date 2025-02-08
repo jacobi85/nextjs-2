cf06d9ba5730840f5edfb078f5add726099a0084
jacobi85 <hello@jacobwestman.com>
2025-02-08 18:48:08 +0100

    connect to postgres

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,9 +1,12 @@
+import { connectToDB } from "@/app/lib/data";
 import { posts } from "@/app/lib/placeholder-data";
 import Post from "@/app/ui/components/posts/Post";
 
-export default function PostsPage() {
+export default async function PostsPage() {
+  const client = await connectToDB();
   return (
     <>
+      {client && <p className="text-green-500">Connected to Database</p>}
       <h1>Posts</h1>
       {posts.map((post) => (
         <Post key={post.id} {...post} isDetailPage={false} />
