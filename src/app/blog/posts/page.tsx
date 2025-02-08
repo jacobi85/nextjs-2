ff3d12065367a4a6e589c4cc811e2806f243df81
jacobi85 <hello@jacobwestman.com>
2025-02-08 21:05:42 +0100

    added api route get and post + form for adding a post

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,14 +1,21 @@
-import { connectToDB } from "@/app/lib/data";
-import { posts } from "@/app/lib/placeholder-data";
+import { Button } from "@/app/ui/components/button";
+import { connectToDB, getPosts } from "@/app/lib/data";
 import Post from "@/app/ui/components/posts/Post";
+import Link from "next/link";
 
 export default async function PostsPage() {
   const client = await connectToDB();
+  const _posts = await getPosts();
   return (
     <>
       {client && <p className="text-green-500">Connected to Database</p>}
+      <Link href="/blog/post/insert">
+        <Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">
+          New +
+        </Button>
+      </Link>
       <h1>Posts</h1>
-      {posts.map((post) => (
+      {_posts!.map((post) => (
         <Post key={post.id} {...post} isDetailPage={false} />
       ))}
     </>
