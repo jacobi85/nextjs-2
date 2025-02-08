45149efd6a7991720b7afbb010429cf919590a66
jacobi85 <hello@jacobwestman.com>
2025-02-08 17:55:02 +0100

    added images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styles from "@/app/ui/styles/home.module.css";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-50">
@@ -21,6 +21,22 @@ export default function Home() {
             >
               Go to Blog
             </Link>
+          </div>
+          <div className="relative flex justify-center items-center">
+            <Image
+              src="/image-desktop.jpeg"
+              width={1000}
+              height={760}
+              className="hidden rounded-lg md:block z-10"
+              alt="Screenshots of the dashboard project showing desktop version"
+            />
+            <Image
+              src="/image-mobile.jpeg"
+              width={560}
+              height={620}
+              className="block rounded-md md:hidden"
+              alt="Screenshot of the dashboard project showing mobile version"
+            />
           </div>
         </div>
       </div>
